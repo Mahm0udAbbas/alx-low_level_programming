b538f9c1197105e4a05ae7e3af21aa1821f3282d
Mahm0udAbbas <mahmoudabbas393@gmail.com>
2023-02-22 17:11:59 -0800

    function task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - main function
- * _putchar : print the alphabet putchar
- * return : always 0.
+ *main - main function
+ *_putchar : print the alphabet putchar
+ *Return: always 0
  */
 int main(void)
 {
@@ -15,5 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-return (0);
+	return (0);
 }
